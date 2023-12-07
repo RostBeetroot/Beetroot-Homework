26bc73a1f86c890e87482f1085c1f89cbe51d7fd
RostBeetroot <142654848+RostBeetroot@users.noreply.github.com>
2023-12-07 22:02:16 +0200

    Update Task 19.2.py

--- Task 19.2.py
+++ Task 19.2.py
@@ -1,5 +1,16 @@
-def in_range(start, end, step=1):
-    while start < end if step > 0 else start > end:
+def in_range(*args):
+    num_args = len(args)
+
+    if num_args == 1:
+        start, end, step = 0, args[0], 1
+    elif num_args == 2:
+        start, end, step = args[0], args[1], 1
+    elif num_args == 3:
+        start, end, step = args
+    else:
+        raise TypeError
+
+    while (step > 0 and start < end) or (step < 0 and start > end):
         yield start
         start += step
 
